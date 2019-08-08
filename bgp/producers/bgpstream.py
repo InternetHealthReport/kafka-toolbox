5c9fd81013a6aa3ecb44262696a9edfd0baf114f
romain.fontugne <romain.fontugne@gmail.com>
2019-08-08 02:21:38 +0000

    clean bgp producer code

--- bgp/producers/bgpstream.py
+++ bgp/producers/bgpstream.py
@@ -13,18 +13,13 @@ def dt2ts(dt):
     return int((dt - datetime(1970, 1, 1)).total_seconds())
 
 
-def getBGPStream(recordType, AF, collectors, startts, endts):
+def getBGPStream(recordType, collectors, startts, endts):
 
     stream = BGPStream()
 
     # recordType is supposed to be ribs or updates
     bgprFilter = "type " + recordType
     
-    if AF == 6:
-        bgprFilter += " and ipversion 6"
-    else:
-        bgprFilter +=  " and ipversion 4"
-
     for c in collectors:
         bgprFilter += " and collector %s " % c
 
@@ -73,58 +68,11 @@ def getElementDict(element):
     return elementDict
 
 
-def pushRIBData(AF, collector, startts, endts):
-
-    stream = getBGPStream("ribs", AF, [collector], startts, endts)
-    topicName = "ihr_bgp_" + collector + "_rib"
-    admin_client = KafkaAdminClient(
-            bootstrap_servers=['kafka1:9092', 'kafka2:9092', 'kafka3:9092'], 
-            client_id='bgp_producer_admin')
-
-    try:
-        topic_list = [NewTopic(name=topicName, num_partitions=1, replication_factor=1)]
-        admin_client.create_topics(new_topics=topic_list, validate_only=False)
-    except:
-        pass
-    admin_client.close()
-
-    stream.start()
-
-    producer = KafkaProducer(
-        bootstrap_servers=['kafka1:9092', 'kafka2:9092', 'kafka3:9092'], 
-        # acks=0, 
-        value_serializer=lambda v: msgpack.packb(v, use_bin_type=True),
-        linger_ms=1000, request_timeout_ms=300000, compression_type='snappy')
-    
-
-    rec = BGPRecord()
-
-    while stream and stream.get_next_record(rec):
-        completeRecord = {}
-        completeRecord["rec"] = getRecordDict(rec)
-        completeRecord["elements"] = []
-
-        recordTimeStamp = rec.time
-
-        recordTimeStamp = int(recordTimeStamp) * 1000
-
-        elem = rec.get_next_elem()
-
-        while(elem):
-            elementDict = getElementDict(elem)
-            completeRecord["elements"].append(elementDict)
-            elem = rec.get_next_elem()
-
-        producer.send(topicName, completeRecord, timestamp_ms=recordTimeStamp)
-
-    producer.close()
-
-
-def pushUpdateData(AF, collector, startts, endts):
+def pushData(record_type, collector, startts, endts):
 
     endts_unix = dt2ts(endts)
-    stream = getBGPStream("updates", AF, [collector], startts, endts)
-    topicName = "ihr_bgp_" + collector + "_update"
+    stream = getBGPStream(record_type, [collector], startts, endts)
+    topicName = "ihr_bgp_" + collector + "_" + record_type
     admin_client = KafkaAdminClient(
             bootstrap_servers=['kafka1:9092', 'kafka2:9092', 'kafka3:9092'], 
             client_id='bgp_producer_admin')
@@ -176,9 +124,7 @@ def pushUpdateData(AF, collector, startts, endts):
 
         if len(completeRecord['elements']):
             producer.send(topicName, completeRecord, timestamp_ms=recordTimeStamp)
-            #producer.send(topicName,recordAsBytes)
             if last_ts != recordTimeStamp:
-                logging.warning("Pushed a record to {}, ts={}, len= {}".format(topicName, recordTimeStamp, len(completeRecord['elements'])))
                 last_ts = recordTimeStamp
 
     producer.close()
@@ -194,8 +140,7 @@ is given then it download data for the current hour."
     parser.add_argument("--collector","-c",help="Choose collector(s) to push data for")
     parser.add_argument("--startTime","-s",help="Choose start time (Format: Y-m-dTH:M:S; Example: 2017-11-06T16:00:00)")
     parser.add_argument("--endTime","-e",help="Choose end time (Format: Y-m-dTH:M:S; Example: 2017-11-06T16:00:00)")
-    parser.add_argument("--af","-a",help="Choose 4 for ipv4, 6 for ipv6")
-    parser.add_argument("--type","-t",help="Choose record type: rib or update")
+    parser.add_argument("--type","-t",help="Choose record type: ribs or updates")
 
     args = parser.parse_args() 
 
@@ -232,16 +177,10 @@ is given then it download data for the current hour."
     logging.info("Arguments: %s" % args)
     logging.warning('start time: {}, end time: {}'.format(timeStart, timeEnd))
 
-    # initialize af
-    if args.af:
-        AF = args.af
-    else:
-        AF = 4
-
     # initialize recordType
     recordType = ""
     if args.type:
-        if args.type in ["rib", "update"]:
+        if args.type in ["ribs", "updates"]:
             recordType = args.type
         else:
             sys.exit("Incorrect type specified; Choose from rib or update")
@@ -249,10 +188,6 @@ is given then it download data for the current hour."
         sys.exit("Record type not specified")
 
     for collector in collectors:
-        if recordType == "rib":
-            logging.warning("Downloading RIB data for {}".format(collector))
-            pushRIBData(AF, collector, timeStart, timeEnd)
+        logging.warning("Downloading {} data for {}".format(recordType, collector))
+        pushData(recordType, collector, timeStart, timeEnd)
         
-        if recordType == "update":
-            logging.warning("Downloading UPDATE data for {}".format(collector))
-            pushUpdateData(AF, collector, timeStart, timeEnd)
