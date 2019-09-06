0b4ded454b9bd18490f4daf6a94fe5704560cecd
romain.fontugne <romain.fontugne@gmail.com>
2019-09-06 05:31:52 +0000

    completed sink for AS hegemony

--- psql/consumers/ASHegemony.py
+++ psql/consumers/ASHegemony.py
@@ -2,16 +2,12 @@ import sys
 import psycopg2
 import psycopg2.extras
 from pgcopy import CopyManager
-from kafka import KafkaConsumer
+from confluent_kafka import Consumer 
 import logging
 from collections import defaultdict
 import json
-import apsw
-import glob
+import msgpack
 from datetime import datetime
-from multiprocessing import JoinableQueue as mpQueue
-from multiprocessing import Process
-from cStringIO import StringIO
 
 def validASN(asn):
     if isinstance(asn,int):
@@ -25,15 +21,13 @@ def validASN(asn):
 
 
 class saverPostgresql(object):
+    """Dumps hegemony results to a Postgresql database. """
 
-    """Dumps only hegemony results to a Postgresql database. """
-
-    def __init__(self, starttime, af, saverQueue, host="localhost", dbname="ihr"):
+    def __init__(self, af, host="localhost", dbname="ihr"):
 
         self.prevts = 0 
         # TODO: get names from Kafka 
-        self.asNames = defaultdict(str, json.load(open("data/asNames.json")))
-        self.currenttime = starttime
+        self.asNames = defaultdict(str, json.load(open("/home/romain/Projects/perso/ashash/data/asNames.json")))
         self.af = af
         self.dataHege = [] 
         self.cpmgr = None
@@ -46,33 +40,60 @@ class saverPostgresql(object):
         self.cursor = self.conn.cursor()
         logging.debug("Connected to the PostgreSQL server")
 
+
+        self.consumer = Consumer({
+            'bootstrap.servers': 'kafka1:9092, kafka2:9092, kafka3:9092',
+            'group.id': 'ihr_hegemony_values_sink0_ipv{}'.format(self.af),
+            'auto.offset.reset': 'earliest',
+            })
+
+        self.consumer.subscribe(['ihr_hegemony_values_ipv{}'.format(self.af)])
+
         self.updateASN()
+
         self.run()
 
     def run(self):
-
-        self.consumer = KafkaConsumer(
-                bootstrap_servers=['kafka1:9092', 'kafka2:9092', 'kafka3:9092'],
-                auto_offset_reset='earliest',
-                key_deserializer=lambda k: int.from_bytes(k, byteorder='big'),
-                value_deserializer=lambda v: msgpack.unpackb(v, raw=False),
-                group_id='ihr_hegemony_reader',
-                )
-
-        self.consumer.subscribe('ihr_hegemony_values_ipv{}'.format(self.af))
+        """
+        Consume data from the kafka topic and save it to the database.
+        """
 
         while True:
-            msg_pack = consumer.poll(10.0)
+            msg = self.consumer.poll(10.0)
             if msg is None:
                 self.commit()
                 continue
+
+            if msg.error():
+                logging.error("Consumer error: {}".format(msg.error()))
+                continue
+
+            msg_val = msgpack.unpackb(msg.value(), raw=False)
+
+            # TODO remove the this: fix for py2/py3 msgpack imcompatibility
+            msg_val['ts'] = msg_val[b'ts']
+            msg_val['scope'] = msg_val[b'scope']
+            msg_val['hege'] = msg_val[b'hege']
+            msg_val['asn'] = msg_val[b'asn']
+
+            if not validASN(msg_val['asn']):
+                if (isinstance(msg_val['asn'], str) 
+                    and msg_val['asn'].startswith('{')
+                    and msg_val['asn'].endswith('{')):
+
+                    # This is an AS set, push results for each ASN in the set
+                        for asn in msg_val['asn'][1:-1].split(','):
+                            if validASN(asn):
+                                msg_val['asn'] = asn
+                                self.save(msg_val)
+
             else:
-                msg = msgpack.unpackb(msg_pack.value(), raw=False)
-                for tp, msgs in msg.items():
-                    for message in msgs:
-                        self.save(message.value)
+                self.save(msg_val)
 
     def updateASN(self):
+        '''
+        Get the list of ASNs from the database
+        '''
 
         self.cursor.execute("SELECT number FROM ihr_asn WHERE ashash=TRUE")
         self.asns = set([x[0] for x in self.cursor.fetchall()])
@@ -80,12 +101,20 @@ class saverPostgresql(object):
 
 
     def save(self, msg):
+        """
+        Buffer the given message and  make sure corresponding ASNs are 
+        registered in the database.
+        """
 
+        if 'ts' not in msg:
+            print(msg)
         # Update the current bin timestamp
         if self.prevts != msg['ts']:
             self.prevts = msg['ts']
             self.currenttime = datetime.utcfromtimestamp(msg['ts'])
             logging.debug("start recording hegemony")
+            self.commit()
+
 
         # Update seen ASNs
         if int(msg['scope']) not in self.asns:
@@ -109,15 +138,19 @@ class saverPostgresql(object):
             self.cursor.execute("UPDATE ihr_asn SET ashash = TRUE where number = %s", (int(msg['asn']),))
 
         # Hegemony values to copy in the database
-        if validASN(msg['asn']) and v!= 0:
+        if msg['hege']!= 0:
             self.dataHege.append((self.currenttime, int(msg['scope']), int(msg['asn']), float(msg['hege']), int(self.af)))
 
     def commit(self):
+        """
+        Push buffered messages to the database and flush the buffer.
+        """
+
         if len(self.dataHege) == 0:
             return
 
         logging.warn("psql: start copy")
-        self.cpmgr.copy(self.dataHege, StringIO)
+        self.cpmgr.copy(self.dataHege)
         self.conn.commit()
         logging.warn("psql: end copy")
         # Populate the table for AS hegemony cone
@@ -138,39 +171,6 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
     af = int(sys.argv[1])
-    directory="newResults%s/" % af
-    
-    for slf in glob.glob(directory+"*.sql"):
-        logging.debug("File: %s" % slf)
-        # slf = "results_2017-03-15 00:00:00.sql"
-        data = defaultdict(lambda: defaultdict(lambda: defaultdict(float)))
-        
-        # Get data from the sqlite db
-        conn = apsw.Connection(slf)
-        cursor = conn.cursor()
-        cursor.execute("SELECT scope, ts, asn, hege FROM hegemony ORDER BY scope")
-
-        for scope, ts, asn, hege in cursor.fetchall():
-            data[scope][ts][asn] = hege
-
-        # Push data to PostgreSQL
-        dt = slf.partition("_")[2]
-        dt = dt.partition(" ")[0]
-        ye, mo, da = dt.split("-")
-        starttime = datetime(int(ye), int(mo), int(da))
-
-        saverQueue = mpQueue(1000)
-        ss = Process(target=saverPostgresql, args=(starttime, af, saverQueue), name="saverPostgresql")
-        ss.start()
-
-        saverQueue.put("BEGIN TRANSACTION;")
-        for scope, allts in data.iteritems():
-            for ts, hege in allts.iteritems():
-                saverQueue.put(("hegemony", (ts, scope, hege)) )
-        saverQueue.put("COMMIT;")
-
-        logging.debug("Finished")
-        saverQueue.join()
-        ss.terminate()
-
+    ss = saverPostgresql(af)
+    ss.start()
 
