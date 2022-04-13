9cb419973505aed2f0b710ab0c7166abdc86a2fd
romain.fontugne <romain.fontugne@gmail.com>
2022-04-13 05:35:35 +0000

    wait longer for prefix hegemony data

--- psql/consumers/ASHegemony_prefix.py
+++ psql/consumers/ASHegemony_prefix.py
@@ -118,7 +118,7 @@ class saverPostgresql(object):
 
             if msg is None:
                 nb_timeout += 1
-                if nb_timeout > 60:
+                if nb_timeout > 120:
                     logging.warning("Time out!")
                     break
                 self.commit()
