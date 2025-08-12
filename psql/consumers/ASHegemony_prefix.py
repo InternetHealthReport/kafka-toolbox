InternetHealthReport/kafka-toolbox

commit 7ae39bf2ae5f00f55269a403d890465e4c8def36

don't set id=0 in ihr_hegemony_prefix

psql/consumers/ASHegemony_prefix.py
@@ -44,7 +44,7 @@ class saverPostgresql(object):
         # conn_string = "host='127.0.0.1' dbname='%s'" % dbname
 
         self.conn = psycopg2.connect(DB_CONNECTION_STRING)
-        columns=("id", "timebin", "prefix", "originasn_id", "asn_id", "country_id", "hege", "rpki_status", "irr_status", "delegated_prefix_status", "delegated_asn_status", "af", "descr", "visibility", "moas")
+        columns=("timebin", "prefix", "originasn_id", "asn_id", "country_id", "hege", "rpki_status", "irr_status", "delegated_prefix_status", "delegated_asn_status", "af", "descr", "visibility", "moas")
         self.cpmgr = CopyManager(self.conn, "ihr_hegemony_prefix", columns)
         self.cursor = self.conn.cursor()
         logging.debug("Connected to the PostgreSQL server")
@@ -279,7 +279,7 @@ class saverPostgresql(object):
                     self.cache['descr'] = descr
 
                 self.dataHege.append([
-                    0, self.currenttime, prefix, int(originasn), int(asn), cc, float(hege), 
+                    self.currenttime, prefix, int(originasn), int(asn), cc, float(hege), 
                     rov_check['rpki']['status'], rov_check['irr']['status'], 
                     rov_check['delegated']['prefix']['status'], rov_check['delegated']['asn']['status'], 
                     self.af, descr, msg['nb_peers']

commit 6b5bab50077c9ffa9f8f184f825461f9ad10a3f8

Update ASHegemony_prefix.py
Fix regression introduced in 7ae39bf2ae5f00f55269a403d890465e4c8def36

psql/consumers/ASHegemony_prefix.py
@@ -298,7 +298,7 @@ class saverPostgresql(object):
         # set visibility in percentage and add moas
         for vec in self.dataHege:
             vec[-1] = 100.0*(vec[-1]/self.nb_peers)
-            rnode = self.rtree.search_best(vec[2])
+            rnode = self.rtree.search_best(vec[1])
             vec.append( len(rnode.data['originasn'])>1 )
 
         logging.warning(f"psql: start copy, ts={self.currenttime}, nb. data points={len(self.dataHege)}")